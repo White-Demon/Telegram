fcde4b24e9779303fc7522c1d2e5139bc59e0da7
White-Demon <Endercool39@ya.ru>
2015-03-12 23:41:35 +0300

    Fixed defect
    It was necessary to remove the identifier "final"

--- TMessagesProj/src/main/java/org/telegram/SQLite/SQLiteException.java
+++ TMessagesProj/src/main/java/org/telegram/SQLite/SQLiteException.java
@@ -10,7 +10,7 @@ package org.telegram.SQLite;
 
 public class SQLiteException extends Exception {
 	private static final long serialVersionUID = -2398298479089615621L;
-	public final int errorCode;
+	public int errorCode;
 	
 	public SQLiteException(int errcode, String msg) {
 		super(msg);
